1de2daaa07b4709353b2a64e31f421702336be2b
FelipeH22 <juan22herrera01@gmail.com>
2023-10-12 15:27:28 -0500

    Some changes

--- video/video_omp.cpp
+++ video/video_omp.cpp
@@ -1,3 +1,61 @@
-//
-// Created by ju4nF on 5/10/2023.
-//
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <omp.h>
+
+int main() {
+    cv::VideoCapture video("video.mp4");
+
+    if (!video.isOpened()) {
+        std::cerr << "Error: Could not open video file." << std::endl;
+        return -1;
+    }
+
+    int outputWidth = 640;  // 420p width
+    int outputHeight = 360; // 420p height
+
+    cv::VideoWriter outputVideo("output_video.mp4", cv::VideoWriter::fourcc('H', '2', '6', '4'), 30, cv::Size(outputWidth, outputHeight), true);
+
+    if (!outputVideo.isOpened()) {
+        std::cerr << "Error: Could not create the output video file." << std::endl;
+        return -1;
+    }
+
+    // Determine the number of threads to use (adjust as needed)
+    int numThreads = omp_get_max_threads();
+
+    while (true) {
+        cv::Mat frame;
+        video >> frame;
+
+        if (frame.empty()) {
+            break;
+        }
+
+        // Parallelize frame processing
+        #pragma omp parallel num_threads(numThreads)
+        {
+            int thread_id = omp_get_thread_num();
+            int start = (frame.rows / numThreads) * thread_id;
+            int end = (thread_id == numThreads - 1) ? frame.rows : (frame.rows / numThreads) * (thread_id + 1);
+
+            cv::Mat framePart = frame.rowRange(start, end);
+            cv::Mat resizedFrame;
+            cv::resize(framePart, resizedFrame, cv::Size(outputWidth, outputHeight));
+
+            #pragma omp critical
+            {
+                outputVideo.write(resizedFrame);
+            }
+        }
+
+        if (cv::waitKey(1) == 'q') {
+            break;
+        }
+    }
+
+    video.release();
+    outputVideo.release();
+    cv::destroyAllWindows();
+
+    return 0;
+}
--- video/video_seq.cpp
+++ video/video_seq.cpp
@@ -1,19 +1,17 @@
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 int main() {
-    // Open the video file
-    cv::VideoCapture video("input_video.mp4");
+    cv::VideoCapture video("video.mp4");
 
     if (!video.isOpened()) {
         std::cerr << "Error: Could not open video file." << std::endl;
         return -1;
     }
 
-    // Define the output resolution
     int outputWidth = 640;  // 420p width
     int outputHeight = 360; // 420p height
 
-    // Create a VideoWriter to save the output video
     cv::VideoWriter outputVideo("output_video.mp4", cv::VideoWriter::fourcc('H', '2', '6', '4'), 30, cv::Size(outputWidth, outputHeight), true);
 
     if (!outputVideo.isOpened()) {
@@ -26,29 +24,15 @@ int main() {
         video >> frame;
 
         if (frame.empty()) {
-            break; // End of video
-        }
-
-        // Resize the frame to the desired resolution
-        cv::resize(frame, frame, cv::Size(outputWidth, outputHeight));
-
-        // Write the resized frame to the output video
-        outputVideo.write(frame);
-
-        // Display the resized frame (optional)
-        cv::imshow("Resized Video", frame);
-
-        // Exit the loop if 'q' is pressed
-        if (cv::waitKey(1) == 'q') {
             break;
-        }
+        }   
+        cv::resize(frame, frame, cv::Size(outputWidth, outputHeight));
+        outputVideo.write(frame);
+        //cv::imshow("Resized Video", frame);
     }
-
-    // Release video objects
     video.release();
     outputVideo.release();
     cv::destroyAllWindows();
 
     return 0;
 }
-//g++ -o video_resizer video_resizer.cpp `pkg-config --cflags --libs opencv4`
